4d87ce1ce404025e864216731a92aff7b1f5a5ea
JonasZaoui <80394799+JonasZaoui@users.noreply.github.com>
2022-05-17 14:03:54 +0200

    Update run_training.sh

--- run_training.sh
+++ run_training.sh
@@ -5,23 +5,16 @@
 #SBATCH --time=96:00:00
 #SBATCH --mem=30000
 #SBATCH --cpus-per-task=10
-#SBATCH --chdir= /network/lustre/iss02/cati/collabs/cmbs/training_jonas
-#SBATCH --output /network/lustre/iss02/cati/collabs/cmbs/training_jonas/log/log_training_jonas-%A
-#SBATCH --error /network/lustre/iss02/cati/collabs/cmbs/training_jonas/log/log_training_jonas-%A
+#SBATCH --chdir /network/lustre/iss02/cati/collabs/cmbs/training_jonas
+#SBATCH --output /network/lustre/iss02/cati/collabs/cmbs/training_jonas/log/log_training_jonas-%A.out
+#SBATCH --error /network/lustre/iss02/cati/collabs/cmbs/training_jonas/log/log_training_jonas-%A.err
 
 #Active l'environnement
-source bv bash
+source /home/jonas.zaoui/Documents/casa_distro/cati_platform/bin/bv bash
 
 #Pour acceder au fichier
 python /network/lustre/iss02/cati/collabs/cmbs/training_jonas/trainer.py
 
 
 
-#JOBNAME = nom de notre job
-# partition = gpu 
-# gres = nb de gpu que je veux
-# time = temps limite d'execution
-# mem = memoire RAM differente de gpu
-# chdir ou se met le code lorsqu'il run
-#source = pour l'environnement
- 
+
